4160bb8e2f0cd5ce51b72176aa4082dd0927f13b
Zeyu Wang <zeyu.wang@kcl.ac.uk>
2017-12-22 01:03:21 +0000

    final1.0
    12.22

--- scripts/main.js
+++ scripts/main.js
@@ -12,9 +12,18 @@ var $root = $('html, body');
 // excluding carousel arrows
 $('a[href^="#"]:not(a[href="#my-slider"])').click(function () {
     $root.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
+        scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 
     return false;
 });
+
+function showHide(checked) {
+    if (checked == true) {
+        $("#to-be-hidden").fadeOut();
+    }
+    else {
+        $("#to-be-hidden").fadeIn();
+    }
+}
 
