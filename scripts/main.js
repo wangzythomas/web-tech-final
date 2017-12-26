94a09f1cb9d9e4cd60aea90e9a55e6230289d89b
Zeyu Wang <zeyu.wang@kcl.ac.uk>
2017-12-26 19:49:25 +0000

    12.26

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// avoiding navigation bar coverage when anchor locating
 // function locateAt(x) {
 //     x = document.getElementById(e);
 //     var y = x.offsetTop;
@@ -8,16 +9,17 @@
 //     window.scrollTo(0,y);
 // }
 
+// scroll smoothly when anchor locating
 var $root = $('html, body');
 // excluding carousel arrows
 $('a[href^="#"]:not(a[href="#my-slider"])').click(function () {
     $root.animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
-
     return false;
 });
 
+// for mobile device: show/hide web content when menu hamburger clicked
 function showHide(checked) {
     if (checked == true) {
         $("#to-be-hidden").fadeOut();
